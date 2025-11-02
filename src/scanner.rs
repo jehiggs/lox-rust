5bd28e3e861e9ddca62c4b7ac881e1b82ce4ef28
James Higgs <jehiggs6@gmail.com>
2025-11-02 10:10:06 +0000

    Update scanner to attach line info to tokens

--- src/scanner.rs
+++ src/scanner.rs
@@ -18,67 +18,71 @@ impl<'a> Scanner<'a> {
         }
         let mut iter = self.source.chars().peekable();
         match iter.next() {
-            Some('{') => self.parse_single_character(Token::LeftBrace),
-            Some('}') => self.parse_single_character(Token::RightBrace),
-            Some('(') => self.parse_single_character(Token::LeftParen),
-            Some(')') => self.parse_single_character(Token::RightParen),
-            Some(';') => self.parse_single_character(Token::Semicolon),
-            Some(',') => self.parse_single_character(Token::Comma),
-            Some('.') => self.parse_single_character(Token::Dot),
-            Some('+') => self.parse_single_character(Token::Plus),
-            Some('-') => self.parse_single_character(Token::Minus),
-            Some('*') => self.parse_single_character(Token::Star),
+            Some('{') => self.parse_single_character(TokenType::LeftBrace),
+            Some('}') => self.parse_single_character(TokenType::RightBrace),
+            Some('(') => self.parse_single_character(TokenType::LeftParen),
+            Some(')') => self.parse_single_character(TokenType::RightParen),
+            Some(';') => self.parse_single_character(TokenType::Semicolon),
+            Some(',') => self.parse_single_character(TokenType::Comma),
+            Some('.') => self.parse_single_character(TokenType::Dot),
+            Some('+') => self.parse_single_character(TokenType::Plus),
+            Some('-') => self.parse_single_character(TokenType::Minus),
+            Some('*') => self.parse_single_character(TokenType::Star),
             Some('/') => {
                 if let Some('/') = iter.next() {
                     let idx = self.source.find('\n').unwrap_or(self.source.len());
                     self.source = &self.source[idx..];
                     self.parse()
                 } else {
-                    self.parse_single_character(Token::Slash)
+                    self.parse_single_character(TokenType::Slash)
                 }
             }
-            Some('!') => self.parse_double_character(Token::Bang, Token::BangEqual),
-            Some('=') => self.parse_double_character(Token::Equal, Token::EqualEqual),
-            Some('>') => self.parse_double_character(Token::Greater, Token::GreaterEqual),
-            Some('<') => self.parse_double_character(Token::Less, Token::LessEqual),
-            Some('"') => Some(self.parse_string()),
-            Some(c) if c.is_numeric() => Some(self.parse_numeric()),
-            Some(_) => Some(self.parse_identifier()),
+            Some('!') => self.parse_double_character(TokenType::Bang, TokenType::BangEqual),
+            Some('=') => self.parse_double_character(TokenType::Equal, TokenType::EqualEqual),
+            Some('>') => self.parse_double_character(TokenType::Greater, TokenType::GreaterEqual),
+            Some('<') => self.parse_double_character(TokenType::Less, TokenType::LessEqual),
+            Some('"') => self.parse_string(),
+            Some(c) if c.is_numeric() => self.parse_numeric(),
+            Some(_) => self.parse_identifier(),
             None => None,
         }
     }
 
-    fn parse_single_character(&mut self, token: Token<'a>) -> Option<Token<'a>> {
+    fn parse_single_character(&mut self, token_type: TokenType<'a>) -> Option<Token<'a>> {
         self.source = &self.source[1..];
-        Some(token)
+        Some(Token::new(self.line, token_type))
     }
 
     fn parse_double_character(
         &mut self,
-        single_token: Token<'a>,
-        double_token: Token<'a>,
+        single_token: TokenType<'a>,
+        double_token: TokenType<'a>,
     ) -> Option<Token<'a>> {
         if let Some('=') = self.source.chars().nth(1) {
             self.source = &self.source[2..];
-            Some(double_token)
+            Some(Token::new(self.line, double_token))
         } else {
             self.source = &self.source[1..];
-            Some(single_token)
+            Some(Token::new(self.line, single_token))
         }
     }
 
-    fn parse_string(&mut self) -> Token<'a> {
+    fn parse_string(&mut self) -> Option<Token<'a>> {
         // Remove the initial quote.
         self.source = &self.source[1..];
+        let current_line = self.line;
         if let Some(idx) = self.source.find('"') {
             let literal = &self.source[0..idx];
             // Need to go past the terminating quote, hence the +1.
             self.source = &self.source[idx + 1..];
             self.line += literal.chars().filter(|&c| c == '\n').count();
-            Token::String(literal)
+            Some(Token::new(current_line, TokenType::String(literal)))
         } else {
             self.source = &self.source[self.source.len()..];
-            Token::Error(UNTERMINATED_ERROR)
+            Some(Token::new(
+                current_line,
+                TokenType::Error(UNTERMINATED_ERROR),
+            ))
         }
     }
 
@@ -98,14 +102,17 @@ impl<'a> Scanner<'a> {
         }
     }
 
-    fn parse_numeric(&mut self) -> Token<'a> {
+    fn parse_numeric(&mut self) -> Option<Token<'a>> {
         let idx = self.number_end_index();
         let number = &self.source[0..idx];
         self.source = &self.source[idx..];
-        Token::Number(number.parse::<f64>().unwrap())
+        Some(Token::new(
+            self.line,
+            TokenType::Number(number.parse::<f64>().unwrap()),
+        ))
     }
 
-    fn parse_identifier(&mut self) -> Token<'a> {
+    fn parse_identifier(&mut self) -> Option<Token<'a>> {
         let ident = if let Some(idx) = self.source.find(|c: char| !c.is_alphanumeric() && c != '_')
         {
             let ident = &self.source[0..idx];
@@ -116,28 +123,30 @@ impl<'a> Scanner<'a> {
             self.source = &self.source[self.source.len()..];
             ident
         };
-        self.check_keyword(ident)
-            .unwrap_or(Token::Identifier(ident))
+        let token_type = self
+            .check_keyword(ident)
+            .unwrap_or(TokenType::Identifier(ident));
+        Some(Token::new(self.line, token_type))
     }
 
-    fn check_keyword(&self, ident: &'a str) -> Option<Token<'a>> {
+    fn check_keyword(&self, ident: &'a str) -> Option<TokenType<'a>> {
         match ident {
-            "and" => Some(Token::And),
-            "class" => Some(Token::Class),
-            "else" => Some(Token::Else),
-            "false" => Some(Token::False),
-            "for" => Some(Token::For),
-            "fun" => Some(Token::Fun),
-            "if" => Some(Token::If),
-            "nil" => Some(Token::Nil),
-            "or" => Some(Token::Or),
-            "print" => Some(Token::Print),
-            "return" => Some(Token::Return),
-            "super" => Some(Token::Super),
-            "this" => Some(Token::This),
-            "true" => Some(Token::True),
-            "var" => Some(Token::Var),
-            "while" => Some(Token::While),
+            "and" => Some(TokenType::And),
+            "class" => Some(TokenType::Class),
+            "else" => Some(TokenType::Else),
+            "false" => Some(TokenType::False),
+            "for" => Some(TokenType::For),
+            "fun" => Some(TokenType::Fun),
+            "if" => Some(TokenType::If),
+            "nil" => Some(TokenType::Nil),
+            "or" => Some(TokenType::Or),
+            "print" => Some(TokenType::Print),
+            "return" => Some(TokenType::Return),
+            "super" => Some(TokenType::Super),
+            "this" => Some(TokenType::This),
+            "true" => Some(TokenType::True),
+            "var" => Some(TokenType::Var),
+            "while" => Some(TokenType::While),
             _ => None,
         }
     }
@@ -152,7 +161,19 @@ impl<'a> Iterator for Scanner<'a> {
 }
 
 #[derive(Debug, PartialEq)]
-pub enum Token<'a> {
+pub struct Token<'a> {
+    pub line: usize,
+    pub token_type: TokenType<'a>,
+}
+
+impl<'a> Token<'a> {
+    fn new(line: usize, token_type: TokenType<'a>) -> Self {
+        Token { line, token_type }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TokenType<'a> {
     LeftParen,
     RightParen,
     LeftBrace,
@@ -196,7 +217,7 @@ pub enum Token<'a> {
 
 #[cfg(test)]
 mod tests {
-    use crate::scanner::{Scanner, Token, UNTERMINATED_ERROR};
+    use crate::scanner::*;
 
     #[test]
     fn parse_single_characters() {
@@ -204,19 +225,19 @@ mod tests {
         let scanner = Scanner::new(text);
         assert_eq!(
             vec![
-                Token::LeftBrace,
-                Token::RightBrace,
-                Token::LeftParen,
-                Token::RightParen,
-                Token::Semicolon,
-                Token::Comma,
-                Token::Dot,
-                Token::Plus,
-                Token::Minus,
-                Token::Star,
-                Token::Slash
+                TokenType::LeftBrace,
+                TokenType::RightBrace,
+                TokenType::LeftParen,
+                TokenType::RightParen,
+                TokenType::Semicolon,
+                TokenType::Comma,
+                TokenType::Dot,
+                TokenType::Plus,
+                TokenType::Minus,
+                TokenType::Star,
+                TokenType::Slash
             ],
-            scanner.collect::<Vec<_>>()
+            scanner.map(|token| token.token_type).collect::<Vec<_>>()
         );
     }
 
@@ -226,50 +247,58 @@ mod tests {
         let scanner = Scanner::new(text);
         assert_eq!(
             vec![
-                Token::Bang,
-                Token::BangEqual,
-                Token::EqualEqual,
-                Token::Equal,
-                Token::GreaterEqual,
-                Token::Greater,
-                Token::LessEqual,
-                Token::Less
+                TokenType::Bang,
+                TokenType::BangEqual,
+                TokenType::EqualEqual,
+                TokenType::Equal,
+                TokenType::GreaterEqual,
+                TokenType::Greater,
+                TokenType::LessEqual,
+                TokenType::Less
             ],
-            scanner.collect::<Vec<_>>()
+            scanner.map(|token| token.token_type).collect::<Vec<_>>()
         );
     }
 
     #[test]
     fn advance_whitespace() {
         let text = "   \n    \n.  \t    ==";
-        let mut scanner = Scanner::new(text);
+        let scanner = Scanner::new(text);
         assert_eq!(
-            vec![Token::Dot, Token::EqualEqual],
-            scanner.by_ref().collect::<Vec<_>>()
+            vec![
+                Token::new(2, TokenType::Dot),
+                Token::new(2, TokenType::EqualEqual)
+            ],
+            scanner.collect::<Vec<_>>()
         );
-        assert_eq!(2, scanner.line);
     }
 
     #[test]
     fn skip_comments() {
         let text = ".+ // this is a comment \n.-";
-        let mut scanner = Scanner::new(text);
+        let scanner = Scanner::new(text);
         assert_eq!(
-            vec![Token::Dot, Token::Plus, Token::Dot, Token::Minus],
-            scanner.by_ref().collect::<Vec<_>>()
+            vec![
+                Token::new(0, TokenType::Dot),
+                Token::new(0, TokenType::Plus),
+                Token::new(1, TokenType::Dot),
+                Token::new(1, TokenType::Minus)
+            ],
+            scanner.collect::<Vec<_>>()
         );
-        assert_eq!(1, scanner.line);
     }
 
     #[test]
     fn parse_string() {
-        let text = "\"string literal \n more literal\"";
-        let mut scanner = Scanner::new(text);
+        let text = "\"string literal \n more literal\" .";
+        let scanner = Scanner::new(text);
         assert_eq!(
-            vec![Token::String("string literal \n more literal")],
-            scanner.by_ref().collect::<Vec<_>>()
+            vec![
+                Token::new(0, TokenType::String("string literal \n more literal")),
+                Token::new(1, TokenType::Dot)
+            ],
+            scanner.collect::<Vec<_>>()
         );
-        assert_eq!(1, scanner.line);
     }
 
     #[test]
@@ -277,7 +306,7 @@ mod tests {
         let text = "\"unterminated literal";
         let scanner = Scanner::new(text);
         assert_eq!(
-            vec![Token::Error(UNTERMINATED_ERROR)],
+            vec![Token::new(0, TokenType::Error(UNTERMINATED_ERROR))],
             scanner.collect::<Vec<_>>()
         );
     }
@@ -288,9 +317,9 @@ mod tests {
         let scanner = Scanner::new(text);
         assert_eq!(
             vec![
-                Token::Number(45.0),
-                Token::Number(12.34),
-                Token::Number(765.0)
+                Token::new(0, TokenType::Number(45.0)),
+                Token::new(0, TokenType::Number(12.34)),
+                Token::new(0, TokenType::Number(765.0))
             ],
             scanner.collect::<Vec<_>>()
         );
@@ -302,22 +331,22 @@ mod tests {
         let scanner = Scanner::new(text);
         assert_eq!(
             vec![
-                Token::And,
-                Token::Class,
-                Token::Else,
-                Token::False,
-                Token::For,
-                Token::Fun,
-                Token::If,
-                Token::Nil,
-                Token::Or,
-                Token::Print,
-                Token::Return,
-                Token::Super,
-                Token::This,
-                Token::True,
-                Token::Var,
-                Token::While
+                Token::new(0, TokenType::And),
+                Token::new(0, TokenType::Class),
+                Token::new(0, TokenType::Else),
+                Token::new(0, TokenType::False),
+                Token::new(0, TokenType::For),
+                Token::new(0, TokenType::Fun),
+                Token::new(0, TokenType::If),
+                Token::new(0, TokenType::Nil),
+                Token::new(0, TokenType::Or),
+                Token::new(0, TokenType::Print),
+                Token::new(0, TokenType::Return),
+                Token::new(0, TokenType::Super),
+                Token::new(0, TokenType::This),
+                Token::new(0, TokenType::True),
+                Token::new(0, TokenType::Var),
+                Token::new(0, TokenType::While)
             ],
             scanner.collect::<Vec<_>>()
         )
@@ -329,10 +358,10 @@ mod tests {
         let scanner = Scanner::new(text);
         assert_eq!(
             vec![
-                Token::Identifier("my_ident"),
-                Token::Identifier("forb"),
-                Token::Identifier("tr"),
-                Token::Identifier("o")
+                Token::new(0, TokenType::Identifier("my_ident")),
+                Token::new(0, TokenType::Identifier("forb")),
+                Token::new(0, TokenType::Identifier("tr")),
+                Token::new(0, TokenType::Identifier("o"))
             ],
             scanner.collect::<Vec<_>>()
         )
@@ -343,7 +372,10 @@ mod tests {
         let text = "123.abc";
         let scanner = Scanner::new(text);
         assert_eq!(
-            vec![Token::Number(123.0), Token::Identifier("abc")],
+            vec![
+                Token::new(0, TokenType::Number(123.0)),
+                Token::new(0, TokenType::Identifier("abc"))
+            ],
             scanner.collect::<Vec<_>>()
         )
     }
